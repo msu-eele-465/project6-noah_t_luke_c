3ece0fc20dbc0af4e9a3b532fb50f365f921afef
Noah Tegeder <noah.tegeder@gmail.com>
2025-04-15 12:17:00 -0600

    Created lcd project

--- controller/main.c
+++ controller/main.c
@@ -33,7 +33,7 @@ void i2c_config(){
                                             // after UCB0TBCNT is reached
 
     UCB0TBCNT = 0x0001;                     // number of bytes to be sent
-    UCB0I2CSA = 0x0A;                       // Slave address
+    UCB0I2CSA = 0x00;                       // Slave address
                                             // Two slaves are being used, 0x0A is the LEDbar, 0x0B is the LCD
                                             // When one of those keys is pressed, update slave address, send data, set back to 0x00  
     // I2C pins, 1.2 SDA, 1.3 SCL
@@ -41,30 +41,157 @@ void i2c_config(){
     P1SEL0 |= BIT2 | BIT3;     
 }
 
-void send_ambient_temp()
-{
-    UCB0I2CSA = 0x0B;        
-    while (UCB0CTL1 & UCTXSTP);
-    data = 0xAD;
-    UCB0CTLW0 |= UCTXSTT;
-    while (UCB0CTL1 & UCTXSTP);
-    __delay_cycles(2000);
-   // data = thousands;
-    UCB0CTLW0 |= UCTXSTT;
-    while (UCB0CTL1 & UCTXSTP);
-    __delay_cycles(2000);
-    //data = hundreds;
-    UCB0CTLW0 |= UCTXSTT;
-    while (UCB0CTL1 & UCTXSTP);
-    __delay_cycles(2000);
-    data = 0b00101110;
-    UCB0CTLW0 |= UCTXSTT;
-    while (UCB0CTL1 & UCTXSTP);
-    __delay_cycles(2000);
-    //data = tens;
-    UCB0CTLW0 |= UCTXSTT;
-    while (UCB0CTL1 & UCTXSTP);
-    __delay_cycles(200000);    
+
+
+void write_set_pattern(){
+    UCB0I2CSA = 0x0B;                      // LCD slave address
+    data = 0x00;                           // Send 0 so LCD writes to top line
+    UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    __delay_cycles(200);
+    int i = 0;
+    for (i = 0; i < sizeof(letters_set_pattern); i++) {
+        data = letters_set_pattern[i];                 // Set data to be transmited to next letter code
+        UCB0CTLW0 |= UCTXSTT;              // Transmit
+        __delay_cycles(200);
+    }
+    __delay_cycles(200);
+    UCB0I2CSA = 0x00;                      // Reset slave address
+}
+
+void write_set_window(){
+    UCB0I2CSA = 0x0B;                      // LCD slave address
+    data = 0x00;                           // Send 0 so LCD writes to top line
+    UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    int i = 0;
+    for (i = 0; i < sizeof(letters_set_window); i++) {
+        data = letters_set_window[i];                 // Set data to be transmited to next letter code
+        UCB0CTLW0 |= UCTXSTT;              // Transmit
+    }
+    UCB0I2CSA = 0x00;                      // Reset slave address
+}
+
+void write_pattern_static(){
+    UCB0I2CSA = 0x0B;                      // LCD slave address
+    data = 0x00;                           // Send 0 so LCD writes to top line
+    UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    int i = 0;
+    for (i = 0; i < sizeof(letters_pattern_static); i++) {
+        data = letters_pattern_static[i];  // Set data to be transmited to next letter code
+        UCB0CTLW0 |= UCTXSTT;              // Transmit
+    }
+    UCB0I2CSA = 0x00;                      // Reset slave address
+}
+
+void write_pattern_toggle(){
+    UCB0I2CSA = 0x0B;                      // LCD slave address
+    data = 0x00;                           // Send 0 so LCD writes to top line
+    UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    int i = 0;
+    for (i = 0; i < sizeof(letters_pattern_toggle); i++) {
+        data = letters_pattern_toggle[i];  // Set data to be transmited to next letter code
+        while (UCB0CTL1 & UCTXSTP);
+        UCB0CTLW0 |= UCTXSTT;              // Transmit
+    }
+    UCB0I2CSA = 0x00;                      // Reset slave address
+}
+
+void write_pattern_up_counter(){
+    UCB0I2CSA = 0x0B;                      // LCD slave address
+    data = 0x00;                           // Send 0 so LCD writes to top line
+    UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    int i = 0;
+    for (i = 0; i < sizeof(letters_pattern_up_counter); i++) {
+        data = letters_pattern_up_counter[i];  // Set data to be transmited to next letter code
+        UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    }
+    UCB0I2CSA = 0x00;                          // Reset slave address
+}
+
+void write_pattern_in_and_out(){
+    UCB0I2CSA = 0x0B;                      // LCD slave address
+    data = 0x00;                           // Send 0 so LCD writes to top line
+    UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    int i = 0;
+    for (i = 0; i < sizeof(letters_pattern_in_and_out); i++) {
+        data = letters_pattern_in_and_out[i];  // Set data to be transmited to next letter code
+        UCB0CTLW0 |= UCTXSTT;                  // Transmit
+    }
+    UCB0I2CSA = 0x00;                          // Reset slave address
+}
+
+void transmit_pattern_led(){
+    UCB0I2CSA = 0x0A;                       // LEDbar slave address
+    switch (key_pressed) {
+        case '0': 
+            data = key_pressed - '0';       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;     
+            write_pattern_static();                   
+            break;
+        case '1': 
+            data = key_pressed - '0';       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;  
+            write_pattern_toggle();                         
+            break;
+        case '2': 
+            data = key_pressed - '0';       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;
+            write_pattern_up_counter();                           
+            break;
+        case '3': 
+            data = key_pressed - '0';       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;
+            write_pattern_in_and_out();                           
+            break;
+    }
+        UCB0I2CSA = 0x00;                   // Reset slave address
+}
+
+void lcd_window_size_transmit(){
+    UCB0I2CSA = 0x0B;                       // LCD slave address
+    switch (key_pressed) {
+        case '0': 
+            data = 0;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                        
+            break;
+        case '1': 
+            data = 1;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;
+            while (UCB0CTL1 & UCTXSTP);                           
+            break;
+        case '2': 
+            data = 2;       
+            UCB0CTLW0 |= UCTXSTT;                           
+            break;
+        case '3': 
+            data = 3;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                           
+            break;
+        case '4': 
+            data = 4;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                        
+            break;
+        case '5': 
+            data = 5;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                           
+            break;
+        case '6': 
+            data = 6;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                           
+            break;
+        case '7': 
+            data = 7;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                           
+            break;
+        case '8': 
+            data = 8;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                           
+            break;
+        case '9': 
+            data = 9;       // Convert char to integer
+            UCB0CTLW0 |= UCTXSTT;                           
+            break;
+    }
+    UCB0I2CSA = 0x00;                       // Reset slave address
 }
 
 int main(void)
@@ -72,9 +199,9 @@ int main(void)
     // Stop WDT
     WDTCTL = WDTPW | WDTHOLD;
 
-    //TB0CCTL0 |= CCIE;                                             // TBCCR0 interrupt enabled
-    //TB0CCR0 = 32;
-    //TB0CTL = TBSSEL__ACLK | MC__UP;                               // ACLK, UP mode
+    TB0CCTL0 |= CCIE;                                             // TBCCR0 interrupt enabled
+    TB0CCR0 = 32;
+    TB0CTL = TBSSEL__ACLK | MC__UP;                               // ACLK, UP mode
 
     // Configure ADC A1 pin
     P1SEL0 |= BIT1;
@@ -96,16 +223,18 @@ int main(void)
 
     // Disable low power mode
     PM5CTL0 &= ~LOCKLPM5;
-    UCB0CTLW0 &= ~UCSWRST; 
+    UCB0CTLW0 &= ~UCSWRST;
 
+    // I2C interrupt
     UCB0IE |= UCTXIE0;  
+
+    // Enable interrupts
     __enable_interrupt();
-    UCB0CTLW0 |= UCTXSTT;
     
     
     while (1)
     {       
-        /*lock_keypad(unlock_code);
+        lock_keypad(unlock_code);
         lock_status = 0;
         UCB0I2CSA = 0x0B;        
         while (UCB0CTL1 & UCTXSTP);
@@ -118,14 +247,28 @@ int main(void)
             real_temp %= 100;
             int tens = (real_temp/10) + 48;
             int ones = (real_temp%10) + 48;
-        }*/
-        data = 0x1;
-        UCB0CTLW0 |= UCTXSTT;
-
-        __delay_cycles(100000);
-        data = 0x2;
-        UCB0CTLW0 |= UCTXSTT;
-        __delay_cycles(100000);
+            data = 0xAD;
+            UCB0CTLW0 |= UCTXSTT;
+            while (UCB0CTL1 & UCTXSTP);
+            __delay_cycles(2000);
+            data = thousands;
+            UCB0CTLW0 |= UCTXSTT;
+            while (UCB0CTL1 & UCTXSTP);
+            __delay_cycles(2000);
+            data = hundreds;
+            UCB0CTLW0 |= UCTXSTT;
+            while (UCB0CTL1 & UCTXSTP);
+            __delay_cycles(2000);
+            data = 0b00101110;
+            UCB0CTLW0 |= UCTXSTT;
+            while (UCB0CTL1 & UCTXSTP);
+            __delay_cycles(2000);
+            data = tens;
+            UCB0CTLW0 |= UCTXSTT;
+            while (UCB0CTL1 & UCTXSTP);
+            __delay_cycles(200000);  
+ 
+        }
     }
 }
 
@@ -177,7 +320,7 @@ void __attribute__ ((interrupt(ADC_VECTOR))) ADC_ISR (void)
             break;
     }
 }
-/*
+
 // Timer B0 interrupt service routine
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector = TIMER0_B0_VECTOR
@@ -189,4 +332,4 @@ void __attribute__ ((interrupt(TIMER0_B0_VECTOR))) Timer_B (void)
 #endif
 {
     ADCCTL0 |= ADCENC | ADCSC;                                    // Sampling and conversion start
-}*/
+}
